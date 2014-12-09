f5e14cafd46f739dec019bc437023e7dffcf4b35
Kyle Fiedler <kyle@kylefiedler.com>
2014-12-09 12:03:13 -0500

    Fade header as scroll

--- javascript/site.js
+++ javascript/site.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
+
   $(".mobile-menu").click(function() {
     $(".navigation-list").slideToggle();
 
     $(".navigation-list > li").click(function(){
       $(".navigation-list").slideHide();
     });
+  });
+
+  $(window).scroll(function(){
+    var scrollVar = $(window).scrollTop();
+    $(".hero").css({"top": .7*scrollVar });
+    $(".hero").css({"opacity":( 500-scrollVar  )/500});
   });
 });
 
